ad9700530b17eb2a571870216739e3ae64558dc5
Bella200625 <bbotellom@unicartagena.edu.co>
2026-04-06 18:35:13 -0500

    Actualizar metodo setPromedio
    - Eliminación del parámetro self y desacoplamiento de la clase para permitir llamadas globales.
    - Sustitución de la asignación manual por una interfaz de terminal dinámica.
    - Implementación de captura de datos (input) para posicion y nuevoPromedio.
    - Mejora en la robustez: validación de rangos de lista y manejo de excepciones (ValueError).

--- python_scripts/punto_2.py
+++ python_scripts/punto_2.py
@@ -9,8 +9,26 @@ class Estudiante:
         print(f"Estudiante: {self.nombre} | Edad: {self.edad} | Promedio: {self.promedio}")
 
 # Método para modificar el promedio 
-    def setPromedio(self, nuevo_promedio):
-        self.promedio = nuevo_promedio
+def setPromedio():
+        if not estudiantes:
+            print(">> No hay estudiantes para modificar.")
+            return
+        try: 
+            posicion = int (input ("Ingresa posicion de la fila del estudiante al que le quieras cambiar el promedio (0, 1, 2): "))
+
+            if 0 <= posicion < len(estudiantes):
+                entrada = input(f"Ingresa el nuevo promedio para {estudiantes[posicion].nombre}: ")
+                nuevoPromedio = float(entrada.replace(",", "."))
+
+                estudiantes[posicion].promedio = nuevoPromedio
+            
+                print(f">> El promedio de {estudiantes[posicion].nombre} ahora es {nuevoPromedio}.")
+            else:
+            # Si el usuario mete un número como 8 y solo hay 3 estudiantes
+                print(f">> ERROR: La posición {posicion} no existe. Intenta con un número entre 0 y {len(estudiantes)-1}.")
+        except ValueError: 
+            print("Invalido.")
+
 
 # se han creado 3 instancias y se han almacenado 
 # en un arreglo/lista.
@@ -28,5 +46,4 @@ for e in estudiantes:
 
 #modificar con metodo
 print("--- Promedio modificado ---")
-estudiantes[1].setPromedio(4.0)
-estudiantes[1].mostrarInfo()
+setPromedio()
