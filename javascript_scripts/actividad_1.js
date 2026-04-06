a82ff02c67b67a2c6d5f531aa09c889ae4a3f1a6
Bella200625 <bbotellom@unicartagena.edu.co>
2026-04-05 21:54:34 -0500

    Declaración e inicialización
    Se aprovecha la naturaleza dinámica de JavaScript para definir y crear 3 instancias de estudiantes con las propiedades: nombre, edad y promedio

---
+++ javascript_scripts/actividad_1.js
@@ -0,0 +1,9 @@
+// declaracion e inicialización este lenguaje es dinamico asi 
+// se puede hacer al mismo tiempo
+const E1 = { nombre: "Adrian", edad: 19, promedio: 5.0 };
+const E2 = { nombre: "Luka", edad: 20, promedio: 5.0 };
+const E3 = { nombre: "Kagami", edad: 19, promedio: 5.0 };
+
+// Guardemos en arreglo
+const estudiantes = [E1, E2, E3];
+
