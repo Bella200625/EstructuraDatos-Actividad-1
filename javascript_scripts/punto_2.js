4f02ab9e7576627ce7a82284ee8c82fae1be302f
Bella200625 <bbotellom@unicartagena.edu.co>
2026-04-06 20:09:06 -0500

    Actualización de Promedios
    Se reemplazó el ingreso manual de datos por una captura dinámica mediante la función prompt integrada con readline-sync.
    
    Se implementó una validación de rango para asegurar que la posición ingresada exista dentro del arreglo estudiantes.length.
    
    Se añadió la limpieza de strings con .replace(",", ".") para permitir que el usuario ingrese decimales con coma sin romper el programa.
    
    Se integró la función parseFloat combinada con isNaN para garantizar que solo se guarden números válidos en el sistema.
    
    Se automatizó la muestra del listado completo mediante un ciclo .forEach() después de cada modificación exitosa.

--- javascript_scripts/punto_2.js
+++ javascript_scripts/punto_2.js
@@ -1,3 +1,5 @@
+const prompt = require('readline-sync').question;
+
 // Declaramos la Clase
 class Estudiante {
     constructor(nombre, edad, promedio) {
@@ -28,7 +30,38 @@ const estudiantes = [
 console.log("--- Listado de Estudiantes ---");
 estudiantes.forEach(e => e.mostrarInfo());
 
+function setPromedio() {
+    if (estudiantes.length === 0) {
+        console.log(" No hay estudiantes para modificar.");
+        return;
+    }
+    // Pedimos la posición
+    let inputPos = prompt("Ingresa posicion de la fila (0, 1, 2): ");
+    let posicion = parseInt(inputPos);
+
+    // Validamos la posición
+    if (!isNaN(posicion) && posicion >= 0 && posicion < estudiantes.length) {
+        
+        // Pedimos el nuevo promedio
+        let newP = prompt(`Ingresa el nuevo promedio para ${estudiantes[posicion].nombre}: `);
+        let nuevoPromedio = parseFloat(newP.replace(",", "."));
+
+        if (!isNaN(nuevoPromedio)) {
+            
+            estudiantes[posicion].promedio = nuevoPromedio;
+            
+            console.log(`El promedio de ${estudiantes[posicion].nombre} ahora es ${nuevoPromedio}.`);
+            console.log(" ");
+            console.log("--- Listado actualizada ---");
+            estudiantes.forEach(e => e.mostrarInfo());
+        } else {
+            console.log("ERROR: El promedio ingresado no es un número válido.");
+        }
+    } else {
+        console.log(`ERROR: La posición ${inputPos} no es válida.`);
+    }
+}
+
 // Modificación con el metodo setPromedio
 console.log("\n--- Actualizando nota ---");
-estudiantes[1].setPromedio(4.0);
-estudiantes[1].mostrarInfo();
+setPromedio()
